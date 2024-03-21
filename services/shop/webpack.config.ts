fa304db1951e55d0aa627320df73873867d31ee1
Янущик Илья <ilya.yanushchik@satellite-soft.ru>
2024-03-21 17:07:07 +0400

    Добавление микрофронтов

---
+++ services/shop/webpack.config.ts
@@ -0,0 +1,36 @@
+import path from 'path';
+import { buildWebpack } from '@packages/build-config';
+
+import type {
+  BuildMode,
+  BuildPaths,
+  BuildPlatform,
+} from '@packages/build-config';
+import type { Configuration } from 'webpack';
+
+interface EnvVariables {
+  mode?: BuildMode;
+  port?: number;
+  isAnalyzer?: boolean;
+  platform?: BuildPlatform;
+}
+
+export default (env: EnvVariables) => {
+  const paths: BuildPaths = {
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
+    output: path.resolve(__dirname, 'build'),
+    src: path.resolve(__dirname, 'src'),
+    public: path.resolve(__dirname, 'public'),
+  };
+
+  const config: Configuration = buildWebpack({
+    port: env.port ?? 3000,
+    mode: env.mode ?? 'development',
+    paths,
+    isAnalyzer: env.isAnalyzer,
+    platform: env.platform ?? 'desktop',
+  });
+
+  return config;
+};
